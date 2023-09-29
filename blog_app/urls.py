2d3791eeaca1475327e5afc194708f81ab7dd9b5
kweku-xvi <nksarps@gmail.com>
2023-09-29 22:13:46 +0000

    url routes for the base project

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -26,6 +26,10 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('login/', user_views.LogIn.as_view(), name='login'),
     path('logout/', user_views.LogOut.as_view(), name='logout'),
+    path('password-reset/', user_views.UserPasswordReset.as_view(), name='password_reset'),
+    path('password-reset/done/', user_views.UserPasswordResetDone.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/token/', user_views.UserPasswordResetConfirm.as_view(), name='password_reset_confirm'),
+    path('password-reset-complete/', user_views.UserPasswordResetComplete.as_view(), name = 'password_reset_complete'),
 ]
 
 if settings.DEBUG:
