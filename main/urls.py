10586bbd53b19ccc56c4bb759aa84f3bff4f1cde
kweku-xvi <nksarps@gmail.com>
2023-09-30 20:14:28 +0000

    modified url route for post_display template

--- main/urls.py
+++ main/urls.py
@@ -3,16 +3,16 @@ from . import views
 from .views import (
     PostListView, 
     PostCreateView, 
-    PostDetailView, 
+    # PostDetailView, 
     PostUpdateView, 
-    PostDeleteView, 
+    PostDeleteView,
     UserPostsListView
     )
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/new/', PostCreateView.as_view(), name='blog-create'),
-    path('post/<int:pk>/detail/', PostDetailView.as_view(), name='blog-detail'),
+    path('post/<int:pk>/detail/', views.post_detail, name='blog-detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='blog-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='blog-delete'),
     path('user/<str:username>/', UserPostsListView.as_view(), name='user-posts'),
